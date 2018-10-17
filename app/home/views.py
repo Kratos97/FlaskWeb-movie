afdb534021d5568651d964076921849b39d3321a
Kratos97 <773668135@qq.com>
2018-10-17 21:33:39 +0800

    更新

---
+++ app/home/views.py
@@ -0,0 +1,77 @@
+# coding:utf8
+from . import home
+from flask import render_template, redirect, url_for
+
+
+# 主页
+# @home.route("/")
+# def index():
+# return render_template("home/index.html")
+
+# 登陆
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+
+# 退出
+@home.route("/logout")
+def logout():
+    return redirect(url_for("home.login"))  # redirect重定向url_for("蓝图.页面")
+
+
+# 注册
+@home.route("/regist")
+def regist():
+    return render_template("home/regist.html")
+
+
+# 用户中心
+@home.route("/user")
+def user():
+    return render_template("home/user.html")
+
+
+# 修改密码
+@home.route("/pwd")
+def pwd():
+    return render_template("home/pwd.html")
+
+
+# 评论记录
+@home.route("/comments")
+def comments():
+    return render_template("home/comments.html")
+
+
+# 登陆日志
+@home.route("/loginlog")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+# 收藏电影
+@home.route("/moviecol")
+def moviecol():
+    return render_template("home/moviecol.html")
+
+#首页
+@home.route("/")
+def index():
+    return render_template("home/index.html")
+
+#轮播效果
+@home.route("/animation")
+def animation():
+    return render_template("home/animation.html")
+
+#搜索
+@home.route("/search")
+def search():
+    return render_template("home/search.html")
+
+#播放
+@home.route("/play")
+def play():
+    return render_template("home/play.html")
+
